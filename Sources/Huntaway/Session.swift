892acb99ac8a8751909044813f3c8792370033b5
tqtifnypmb <tqtifnypmb@gmail.com>
2016-03-15 14:12:55 +0800

    Add License

--- Sources/Huntaway/Session.swift
+++ Sources/Huntaway/Session.swift
@@ -1,10 +1,28 @@
 //
 //  Session.swift
-//  XcodeProject
+//  Huntaway
 //
-//  Created by Tqtifnypmb on 3/9/16.
-//  Copyright © 2016 Tqtifnypmb. All rights reserved.
+//  The MIT License (MIT)
 //
+//  Copyright (c) 2016 tqtifnypmb
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 
 import Foundation
 
@@ -182,9 +200,7 @@ final class Session: NSObject, NSURLSessionDelegate, NSURLSessionTaskDelegate, N
         }
         
         resp.markCompleted()
-        if let completeHandler = resp.completeHandler {
-            completeHandler(resp: resp, error: error)
-        }
+        resp.completeHandler?(resp: resp, error: error)
     }
     
     func URLSession(session: NSURLSession, task: NSURLSessionTask, didReceiveChallenge challenge: NSURLAuthenticationChallenge, completionHandler: (NSURLSessionAuthChallengeDisposition, NSURLCredential?) -> Void) {
@@ -319,9 +335,7 @@ final class Session: NSObject, NSURLSessionDelegate, NSURLSessionTaskDelegate, N
         }
         
         resp.markCompleted()
-        if let completeHandler = resp.downloadHandler {
-            completeHandler(url: location)
-        }
+        resp.downloadHandler?(url: location)
     }
     
     func URLSession(session: NSURLSession, downloadTask: NSURLSessionDownloadTask, didResumeAtOffset fileOffset: Int64, expectedTotalBytes: Int64) {
